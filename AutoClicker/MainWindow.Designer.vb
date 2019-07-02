13476e078d54d2bd2132056ae8f229aedcdf9e0d
U1758957 <u1758957@unimail.hud.ac.uk>
2019-07-02 21:59:08 +0100

    Finished Project

---
+++ AutoClicker/MainWindow.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainWindow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ClickTimeLabel = New System.Windows.Forms.Label()
+        Me.HourBox = New System.Windows.Forms.TextBox()
+        Me.MinuteBox = New System.Windows.Forms.TextBox()
+        Me.SecondBox = New System.Windows.Forms.TextBox()
+        Me.MillisecondBox = New System.Windows.Forms.TextBox()
+        Me.HourLabel = New System.Windows.Forms.Label()
+        Me.MinuteLabel = New System.Windows.Forms.Label()
+        Me.SecondLabel = New System.Windows.Forms.Label()
+        Me.MillisecondLabel = New System.Windows.Forms.Label()
+        Me.AutoClickButton = New System.Windows.Forms.Button()
+        Me.TestButton = New System.Windows.Forms.Button()
+        Me.StopButton = New System.Windows.Forms.Button()
+        Me.TimeMeasureLabel = New System.Windows.Forms.Label()
+        Me.TestButtonClicksLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ClickTimeLabel
+        '
+        Me.ClickTimeLabel.AutoSize = True
+        Me.ClickTimeLabel.Location = New System.Drawing.Point(12, 17)
+        Me.ClickTimeLabel.Name = "ClickTimeLabel"
+        Me.ClickTimeLabel.Size = New System.Drawing.Size(106, 13)
+        Me.ClickTimeLabel.TabIndex = 0
+        Me.ClickTimeLabel.Text = "Time Between Clicks"
+        '
+        'HourBox
+        '
+        Me.HourBox.Location = New System.Drawing.Point(125, 54)
+        Me.HourBox.Name = "HourBox"
+        Me.HourBox.Size = New System.Drawing.Size(100, 20)
+        Me.HourBox.TabIndex = 1
+        Me.HourBox.Text = "0"
+        Me.HourBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'MinuteBox
+        '
+        Me.MinuteBox.Location = New System.Drawing.Point(284, 54)
+        Me.MinuteBox.Name = "MinuteBox"
+        Me.MinuteBox.Size = New System.Drawing.Size(100, 20)
+        Me.MinuteBox.TabIndex = 2
+        Me.MinuteBox.Text = "0"
+        Me.MinuteBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'SecondBox
+        '
+        Me.SecondBox.Location = New System.Drawing.Point(448, 54)
+        Me.SecondBox.Name = "SecondBox"
+        Me.SecondBox.Size = New System.Drawing.Size(100, 20)
+        Me.SecondBox.TabIndex = 3
+        Me.SecondBox.Text = "0"
+        Me.SecondBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'MillisecondBox
+        '
+        Me.MillisecondBox.Location = New System.Drawing.Point(621, 54)
+        Me.MillisecondBox.Name = "MillisecondBox"
+        Me.MillisecondBox.Size = New System.Drawing.Size(100, 20)
+        Me.MillisecondBox.TabIndex = 4
+        Me.MillisecondBox.Text = "0"
+        Me.MillisecondBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'HourLabel
+        '
+        Me.HourLabel.AutoSize = True
+        Me.HourLabel.Location = New System.Drawing.Point(80, 61)
+        Me.HourLabel.Name = "HourLabel"
+        Me.HourLabel.Size = New System.Drawing.Size(40, 13)
+        Me.HourLabel.TabIndex = 5
+        Me.HourLabel.Text = "Hour/s"
+        '
+        'MinuteLabel
+        '
+        Me.MinuteLabel.AutoSize = True
+        Me.MinuteLabel.Location = New System.Drawing.Point(230, 61)
+        Me.MinuteLabel.Name = "MinuteLabel"
+        Me.MinuteLabel.Size = New System.Drawing.Size(49, 13)
+        Me.MinuteLabel.TabIndex = 6
+        Me.MinuteLabel.Text = "Minute/s"
+        '
+        'SecondLabel
+        '
+        Me.SecondLabel.AutoSize = True
+        Me.SecondLabel.Location = New System.Drawing.Point(389, 61)
+        Me.SecondLabel.Name = "SecondLabel"
+        Me.SecondLabel.Size = New System.Drawing.Size(54, 13)
+        Me.SecondLabel.TabIndex = 7
+        Me.SecondLabel.Text = "Second/s"
+        '
+        'MillisecondLabel
+        '
+        Me.MillisecondLabel.AutoSize = True
+        Me.MillisecondLabel.Location = New System.Drawing.Point(553, 61)
+        Me.MillisecondLabel.Name = "MillisecondLabel"
+        Me.MillisecondLabel.Size = New System.Drawing.Size(63, 13)
+        Me.MillisecondLabel.TabIndex = 8
+        Me.MillisecondLabel.Text = "Milliscond/s"
+        '
+        'AutoClickButton
+        '
+        Me.AutoClickButton.Location = New System.Drawing.Point(249, 136)
+        Me.AutoClickButton.Name = "AutoClickButton"
+        Me.AutoClickButton.Size = New System.Drawing.Size(75, 23)
+        Me.AutoClickButton.TabIndex = 9
+        Me.AutoClickButton.Text = "Autoclick!"
+        Me.AutoClickButton.UseVisualStyleBackColor = True
+        '
+        'TestButton
+        '
+        Me.TestButton.Location = New System.Drawing.Point(476, 136)
+        Me.TestButton.Name = "TestButton"
+        Me.TestButton.Size = New System.Drawing.Size(75, 23)
+        Me.TestButton.TabIndex = 10
+        Me.TestButton.Text = "Test"
+        Me.TestButton.UseVisualStyleBackColor = True
+        '
+        'StopButton
+        '
+        Me.StopButton.Location = New System.Drawing.Point(713, 118)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(75, 58)
+        Me.StopButton.TabIndex = 11
+        Me.StopButton.Text = "STOP"
+        Me.StopButton.UseVisualStyleBackColor = True
+        '
+        'TimeMeasureLabel
+        '
+        Me.TimeMeasureLabel.AutoSize = True
+        Me.TimeMeasureLabel.Location = New System.Drawing.Point(551, 166)
+        Me.TimeMeasureLabel.Name = "TimeMeasureLabel"
+        Me.TimeMeasureLabel.Size = New System.Drawing.Size(0, 13)
+        Me.TimeMeasureLabel.TabIndex = 12
+        '
+        'TestButtonClicksLabel
+        '
+        Me.TestButtonClicksLabel.AutoSize = True
+        Me.TestButtonClicksLabel.Location = New System.Drawing.Point(476, 167)
+        Me.TestButtonClicksLabel.Name = "TestButtonClicksLabel"
+        Me.TestButtonClicksLabel.Size = New System.Drawing.Size(0, 13)
+        Me.TestButtonClicksLabel.TabIndex = 13
+        '
+        'MainWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 192)
+        Me.Controls.Add(Me.TestButtonClicksLabel)
+        Me.Controls.Add(Me.TimeMeasureLabel)
+        Me.Controls.Add(Me.StopButton)
+        Me.Controls.Add(Me.TestButton)
+        Me.Controls.Add(Me.AutoClickButton)
+        Me.Controls.Add(Me.MillisecondLabel)
+        Me.Controls.Add(Me.SecondLabel)
+        Me.Controls.Add(Me.MinuteLabel)
+        Me.Controls.Add(Me.HourLabel)
+        Me.Controls.Add(Me.MillisecondBox)
+        Me.Controls.Add(Me.SecondBox)
+        Me.Controls.Add(Me.MinuteBox)
+        Me.Controls.Add(Me.HourBox)
+        Me.Controls.Add(Me.ClickTimeLabel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "MainWindow"
+        Me.Text = "DocMarker Autoclicker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ClickTimeLabel As Label
+    Friend WithEvents HourBox As TextBox
+    Friend WithEvents MinuteBox As TextBox
+    Friend WithEvents SecondBox As TextBox
+    Friend WithEvents MillisecondBox As TextBox
+    Friend WithEvents HourLabel As Label
+    Friend WithEvents MinuteLabel As Label
+    Friend WithEvents SecondLabel As Label
+    Friend WithEvents MillisecondLabel As Label
+    Friend WithEvents AutoClickButton As Button
+    Friend WithEvents TestButton As Button
+    Friend WithEvents StopButton As Button
+    Friend WithEvents TimeMeasureLabel As Label
+    Friend WithEvents TestButtonClicksLabel As Label
+End Class
---
+++ AutoClicker/MainWindow.vb
@@ -0,0 +1,150 @@
+﻿Imports System.Globalization
+Imports System.Text.RegularExpressions
+Imports System.Threading
+Public Class MainWindow
+
+
+    Dim totalTime As Double = 0
+    Dim timeUnits As String() = {"h", "m", "s", "ms"}
+    Dim totalTestClicks As UInteger = 0
+    Dim testButtonClickStartTime As DateTime
+    Dim testButtonClickElapsedTime As TimeSpan
+    Dim currentCountDownTime As UInteger = 5
+    Dim stopClicking As Boolean = False
+
+    ''' <summary>
+    ''' Converts a unit of time (hours, minutes, seconds, etc) into milliseconds
+    ''' </summary>
+    ''' <param name="x">The time to convert</param>
+    ''' <param name="unit">The unit of said time</param>
+    Private Sub TimeConverter(ByVal x As UInteger, ByRef unit As String)
+        Select Case unit
+            Case "h"
+                x = x * 60 * 60 * 1000
+            Case "m"
+                x = x * 60 * 1000
+            Case "s"
+                x *= 1000
+        End Select
+        totalTime += x
+    End Sub
+
+    ''' <summary>
+    ''' Spawns as a separate thread, this will autoclick the mouse at a time interval defined by the user. Has a couple of safety features to stop clicking either by pressing the STOP button in the program, or the spacebar key on the keyboard
+    ''' </summary>
+    Private Sub ClickAuto()
+        While (Not stopClicking)
+            If NativeMethods.GetAsyncKeyState(&H20) Then
+                stopClicking = True
+            Else
+                If currentCountDownTime <= 0 Then
+                    NativeMethods.mouse_event(&H2, 0, 0, 0, 0) ' &H2 is left click down
+                    NativeMethods.mouse_event(&H4, 0, 0, 0, 0) ' &H4 is left click up
+                    Thread.Sleep(totalTime)
+                Else
+                    Thread.Sleep(100) ' This is here to stop one of your CPU cores screaming while waiting for the countdown
+                End If
+            End If
+        End While
+    End Sub
+
+    ''' <summary>
+    ''' Does a 5 second countdown after you start the autoclicking, so you have time to get to the thing you want to autoclick on. Also spawns as a separate thread
+    ''' </summary>
+    Private Sub ClickCountDown()
+        For x As Integer = 1 To 5
+            If Not stopClicking Then
+                Thread.Sleep(1000)
+                currentCountDownTime -= 1
+            Else
+                Exit For
+            End If
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' A Button that says "Autoclick!". When you click this button, the time input is parsed, a countdown is then started and at the same time the auto clicker is activated.
+    ''' </summary>
+    ''' <param name="sender">Component of vb.NET for button listening</param>
+    ''' <param name="e">Component of vb.NET for button listening</param>
+    Private Sub AutoClickButton_Click(sender As Object, e As EventArgs) Handles AutoClickButton.Click
+        AutoClickButton.Enabled = False
+        Dim currentTimeValue As UInteger
+        Dim currentTimeString As String = "0"
+        For Each timeUnit As String In timeUnits
+            Select Case timeUnit
+                Case "h"
+                    currentTimeString = HourBox.Text
+                Case "m"
+                    currentTimeString = MinuteBox.Text
+                Case "s"
+                    currentTimeString = SecondBox.Text
+                Case "ms"
+                    currentTimeString = MillisecondBox.Text
+            End Select
+            If Not Regex.IsMatch(currentTimeString, "^[0-9 ]+$") Then
+                StopButton.PerformClick()
+                Exit Sub
+            End If
+            Dim parseSuccess = UInteger.TryParse(currentTimeString, currentTimeValue)
+            If Not parseSuccess Then
+                StopButton.PerformClick()
+                Exit Sub
+            End If
+            TimeConverter(currentTimeValue, timeUnit)
+        Next
+        If (totalTime > 0) Then
+            stopClicking = False
+            Dim countDownThread As New Thread(AddressOf ClickCountDown)
+            Dim clickThread As New Thread(AddressOf ClickAuto)
+            countDownThread.Start()
+            clickThread.Start()
+        Else
+            StopButton.PerformClick()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Hover the mouse over this button when autoclicking, and it will click it. Shows how many clicks have happened, and the time it takes to do 1 click, as a way to see that everything is working, and the (rough) time per click
+    ''' </summary>
+    ''' <param name="sender">Component of vb.NET for button listening</param>
+    ''' <param name="e">Component of vb.NET for button listening</param>
+    Private Sub TestButton_Click(sender As Object, e As EventArgs) Handles TestButton.Click
+        totalTestClicks += 1
+        testButtonClickElapsedTime = Now.Subtract(testButtonClickStartTime)
+        testButtonClickStartTime = Now
+        TimeMeasureLabel.Text = testButtonClickElapsedTime.TotalSeconds.ToString("0.00000" & "s", CultureInfo.CurrentCulture)
+        TestButtonClicksLabel.Text = totalTestClicks
+    End Sub
+
+    ''' <summary>
+    ''' Click this button to stop autoclicking, resets everything
+    ''' </summary>
+    ''' <param name="sender">Component of vb.NET for button listening</param>
+    ''' <param name="e">Component of vb.NET for button listening</param>
+    Private Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+        stopClicking = True
+        totalTime = 0
+        totalTestClicks = 0
+        TimeMeasureLabel.Text = ""
+        TestButtonClicksLabel.Text = ""
+        currentCountDownTime = 5
+        AutoClickButton.Enabled = True
+    End Sub
+
+    Private Sub MainWindow_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        stopClicking = True
+    End Sub
+
+    ''' <summary>
+    ''' NativeMethods class for accessing libraries
+    ''' </summary>
+    Friend Class NativeMethods
+        Private Sub New()
+        End Sub
+        Friend Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As IntPtr)
+        Friend Declare Function GetAsyncKeyState Lib "user32" (ByVal vKey As Integer) As Short
+
+    End Class
+
+End Class
